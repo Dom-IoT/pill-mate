bfb22b65e0049f284081356059736b3b6f3702c4
Floris Bartra <floris.bartra@gmail.com>
2025-01-28 17:26:02 +0100

    User Home Assistant database in the backend

--- pill_mate/backend/src/sequelize.ts
+++ pill_mate/backend/src/sequelize.ts
@@ -4,7 +4,12 @@ import { logger } from './logger';
 
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: ':memory:',
+    storage: '/homeassistant/home-assistant_v2.db',
     logging: message => logger.log({ level: 'debug', message: `sequelize: ${message}` }),
     models: [],
+    hooks: {
+        beforeDefine: (_, model) => {
+            model.tableName = `pillmate_${model.name?.plural}`;
+        },
+    },
 });
