e866be08a0242020ab43fd1a51942b5bd46d9a4a
Augustin <bouyer.augustin@gmail.com>
2025-02-12 22:45:07 +0100

    Ajout d'une checkbox au milieu de l'application + adaptation à la taille de l'écran

--- pill_mate/frontend/src/Dashboard.tsx
+++ pill_mate/frontend/src/Dashboard.tsx
@@ -1,40 +1,75 @@
-import React from "react";
-import { Calendar, ClipboardList, Scan, Settings } from "lucide-react";
+import React, { useState } from "react";
+import {
+  Calendar,
+  ClipboardList,
+  Scan,
+  Settings,
+  Square,
+  SquareCheckBig,
+} from "lucide-react";
 import styles from "./Dashboard.module.css"; // Import du fichier CSS
 
 const Dashboard: React.FC = () => {
+  const iconSize = "clamp(4rem, 10vw, 8rem)";
   const buttons = [
     {
       label: "Calendrier",
-      icon: <Calendar size={200} />,
+      icon: <Calendar size={iconSize} />,
       className: styles.blue,
     },
     {
       label: "Gestion des stocks",
-      icon: <ClipboardList size={200} />,
+      icon: <ClipboardList size={iconSize} />,
       className: styles.green,
     },
-    { label: "Ordonnance", icon: <Scan size={200} />, className: styles.red },
+    {
+      label: "Ordonnance",
+      icon: <Scan size={iconSize} />,
+      className: styles.red,
+    },
     {
       label: "Paramètres",
-      icon: <Settings size={200} />,
+      icon: <Settings size={iconSize} />,
       className: styles.orange,
     },
   ];
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
 
   return (
-    <div className={styles.container}>
-      {buttons.map((btn, index) => (
-        <div key={index} className={`${styles.button} ${btn.className}`}>
-          <div
-            className={styles.icon}
-            onClick={() => console.log(`${btn.label} cliqué !`)}
-          >
-            {btn.icon}
+    <div>
+      <div className={styles.checkbox}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            style={{ display: "none" }}
+          />
+          <div className={styles.checkboxIcon}>
+            {isChecked ? (
+              <SquareCheckBig size={iconSize} />
+            ) : (
+              <Square size={iconSize} />
+            )}
           </div>
-          <span>{btn.label}</span>
-        </div>
-      ))}
+        </label>
+      </div>
+      <div className={styles.container}>
+        {buttons.map((btn, index) => (
+          <div key={index} className={`${styles.button} ${btn.className}`}>
+            <div
+              className={styles.icon}
+              onClick={() => console.log(`${btn.label} cliqué !`)}
+            >
+              {btn.icon}
+            </div>
+            <span>{btn.label}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
